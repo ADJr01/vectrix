0e49b51a9b119300147a8190427eef66135c3986
Rahat Adnan <sthep90@gmail.com>
2025-07-15 01:59:22 +0600

    column & row fetching done

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ int main() {
         9, -3,9,0,0,1,2,3,3,3
     };
     int B[] = {
-        1, -5,
-        2, -2,
-        1, 0
+        1, 9, 6,
+        2, -2, -7,
+        1, 0, 3
     };
 
-    multiply_n_x_n_matrix(A,B,2,3,10,2,2,3);
+    multiply_n_x_n_matrix(A,B,2,3,10,3,3,3);
 
 }
 
@@ -21,16 +21,16 @@ int main() {
 //? means chaos state
 void multiply_n_x_n_matrix(const int* mat_a,const int* mat_b,int total_row_a,int total_row_b,int row_size_a,int row_size_b,const int total_column_b,int column_size_b) {
    // if (row_size_a != column_size_b) throw std::invalid_argument("AxB Matrix multiplication is not possible. in equal row_size_a & column_size_b");
-    /**  ok
+    //*  ok
     for (int row = 0; row < total_row_a; row++) { // * we know the total number of row in matrix A
         const auto current_row = (row*row_size_a);
             for (int items_from_row = 0;items_from_row<row_size_a;items_from_row++) { // * we know the length of each row from matrix A
                 const auto row_index_digit = mat_a[current_row+items_from_row];
                 printf("(%d) ",row_index_digit);
             }
-        printf("\n");
+        printf("==row\n");
     }
-    */
+
     printf("\n");
     // ? maybe working
     const int total_iteration = column_size_b * row_size_b;
@@ -38,7 +38,7 @@ void multiply_n_x_n_matrix(const int* mat_a,const int* mat_b,int total_row_a,int
         for (int column_item = column_head;column_item < total_iteration; column_item+=row_size_b) {
             printf("%d ",mat_b[column_item]);
         }
-        printf("\n");
+        printf("==column\n");
     }
 }
 
